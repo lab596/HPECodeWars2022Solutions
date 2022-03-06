50f5f772400bd209674988a0ec8ee1b7b65e277d
lab596 <86579136+lab596@users.noreply.github.com>
2022-03-05 21:56:55 -0800

    Update Prob06.java

--- Prob06.java
+++ Prob06.java
@@ -1,4 +1,30 @@
 //Prob06:
+/*
+Maria just received some money and has decided she would like some more.
+She's going to save it in the bank and get interest back. Using the Simple Interest
+Formula, figure out how much interest she'll get paid.
+The Simple Interest Formula is:
+I = Prt
+...where P = the Principal (the initial amount of money),
+r = the rate of return per year,
+t = the number of years.
+For example, if P = $10000, r = .04 (or 4% per year), and t = 5 years, the interest I
+= ($10000)(.04)(5) = $2000, so she gets $2000 in interest in 5 years!
+Given P, r, and t, find I.
+Input
+The input has 3 lines. The first is an integer showing the starting Principal amount. The second is the rate of
+return per year, as a decimal fraction beginning with "0.". The third is an integer, showing the number of years.
+10000
+0.04
+5
+Output
+Print the Interest as a decimal fraction with 2 decimal places. Do not round up! Instead truncate any digits past 2
+decimal places.
+2000.00
+Discussion
+For Output 1, Prt = 4305.31875. We truncate to 4305.31. For Output 2, Prt = 13362.966. We truncate to
+13362.96.
+*/
 
 import java.util.Scanner;
 import java.lang.Math;
