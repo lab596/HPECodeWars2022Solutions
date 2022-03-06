3abdc5e4c117ae57b52d8c47cc446df58b0ec9a9
lab596 <86579136+lab596@users.noreply.github.com>
2022-03-05 21:54:23 -0800

    Update Prob03.java

--- Prob03.java
+++ Prob03.java
@@ -1,4 +1,37 @@
 //Prob 03:
+/*
+Basketball is full of advanced shooting metrics and one of
+these metrics is true shooting percentage.
+Though it is not a perfect statistical measurement, the main
+idea of TS% is that it is a measure of scoring efficiency
+based on the number of points scored over the number of
+possessions in which a player attempted to score. In simple
+terms, true shooting percentage can be thought of as points
+per shot.
+True shooting percentage is calculated by using the formula:
+TS% = 100 * PTS/(2(FGA + (0.44 * FTA)))
+Where:
+PTS = points scored
+FGA = field goal attempts (shots taken)
+FTA = free throw attempts
+Write a program to calculate the true shooting percentage of a player given the PTS scored, FGA and FTA.
+Input
+The input consists of three whole numbers in one line separated by spaces: the total PTS, total FGA, and total
+FTA.
+98 71 43
+Output
+Use the PTS, FGA, and FTA to calculate the TS%. Print the true shooting percentage, including two decimal
+places rounded, and append a '%' sign. [1]
+54.49%
+Discussion
+Round to nearest hundredth. The denominator may contain a 0 but will not result in 0. The result may be more
+than 100%.
+[1] Rounding should follow the rule of: round up if the last number is 5-9, leave the hundreths place (the 2nd digit)
+unchanged if the last number is 0-4.
+E.G.
+0.465 -> 0.47
+0.464 -> 0.46
+*/
 
 import java.util.Scanner;
 import java.lang.Math;
