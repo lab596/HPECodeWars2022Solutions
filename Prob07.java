47f534739a57a366a0f6592e51d28e40868d467c
lab596 <86579136+lab596@users.noreply.github.com>
2022-03-05 21:57:32 -0800

    Update Prob07.java

--- Prob07.java
+++ Prob07.java
@@ -1,4 +1,45 @@
 //Prob07:
+/*
+Every contestant is allowed one punt, pass, and
+kick in this thrilling competition. Scoring is based on
+both distance and accuracy. The score is also
+determined from where a contestant's ball first
+makes contact with the ground, therefore bounces
+or rolls are not part of the distance. If a contestant
+passes their ball 121 feet, but it went wide of the
+measuring tape by 30 feet, their final score for
+passing is 91 feet; the score is the difference
+between the two measures.
+The distances of the balls are measured and
+rounded to the nearest foot. A person can not score
+less than 0. There are 5 contestants and no ball can
+go farther than 300 feet or be off the mark by more
+than 300 feet. A participant's final score is the total
+of the three events (Punt, Pass, and Kick). For
+example, a participant scores 30 feet for punting, 50
+feet for passing and 22 feet for kicking, the
+participant's final score is 102 feet
+Write a program that displays the results of the competition given the distance and accuracy of each category and also
+the contestant's name.
+All input and output will be in whole feet. The range of numbers for distance and accuracy is 0 to 300.
+Input
+The input consists of 3 pairs of numbers and a name. For the pairs of numbers, one pair is for punting, another for
+passing, and last for kicking. The first number in each pair is the distance and the second number is how far off the
+mark the ball was.
+Use the contestants' names and scores to compute a list of results.
+40 20 61 17 52 13 Justin
+30 40 60 50 100 10 Aaron
+70 10 53 13 42 19 Lamar
+60 20 53 10 70 20 Patrick
+40 5 60 0 9 13 Tom
+Output
+Print an ordered list of results with the name in the first column and total distance in the second column.
+Patrick 133
+Lamar 123
+Justin 103
+Aaron 100
+Tom 95
+*/
 
 import java.util.Scanner;
 import java.util.ArrayList;
