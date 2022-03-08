8b004ec3cdc5aa303c73659e41e968f6b9a858d3
lab596 <86579136+lab596@users.noreply.github.com>
2022-03-08 10:56:22 -0800

    Create Prob00.java

---
+++ Prob00.java
@@ -0,0 +1,20 @@
+//Prob00:
+/*
+You've been invited up on stage to sing
+along with Beyoncé as she sings her
+hit single, Formation. There is a
+teleprompter at the foot of the stage
+with the lyrics showing you the next
+line.
+Input
+There is no data file to read in for this (and only this) problem.
+Output
+Print to the screen the following line from the song.
+Okay, ladies, now let's get in formation, prove to me you got some coordination.
+*/
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Okay, ladies, now let's get in formation, prove to me you got some coordination.");
+  }
+}
