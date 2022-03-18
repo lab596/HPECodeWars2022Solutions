d8e4892b6f56c30af738dace87f10a9448ff3cfc
lab596 <86579136+lab596@users.noreply.github.com>
2022-03-18 10:01:13 -0700

    Update Prob13.java

--- Prob13.java
+++ Prob13.java
@@ -4,21 +4,17 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.*;
 class Main {
-  public static String format(ArrayList<String> nums){
-    String result=" ";
+  public static String[] format(ArrayList<String> nums){
+    String[] nums2  = new String[nums.size()];
     for(int i=0;i<nums.size();i++){
-      if(i==nums.size()-1){
-        result = result + "#";
-      }
-      result= result + "#,";
-      
+      nums2[i] = "#";
     }
-    return result;
+    return nums2;
   }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
-    int y = sc.nextInt();
+    int x = Integer.parseInt(sc.nextLine());
+    int y = Integer.parseInt(sc.nextLine());
     ArrayList<ArrayList<String> > outter = new ArrayList<ArrayList<String> >();
     for(int i = 0; i<y; i++){
       ArrayList<String> inner = new ArrayList<String>(x);
@@ -29,18 +25,21 @@ class Main {
     }
     String test;
     while(!(test = sc.nextLine()).equals("END")){
+      System.out.println("test: " + test);
       //String str = sc.nextLine();
       String array1[]= test.split(":");
-      int ycoord = Integer.parseInt(array1[0]);
-      String array2[]= array1[1].split(" ");
+      System.out.println(Arrays.toString(array1));
+      int ycoord = Integer.parseInt(array1[0].strip());
+      String array2[]= array1[1].strip().split(" ");
       //System.out.println(outter.get(ycoord));
       for(int i=0;i<array2.length;i++){
         ArrayList<String> inside = new ArrayList<String>(Arrays.asList(format(outter.get(ycoord))));
-        String val = inside.get(Integer.parseInt(array2[i]));
-        val = " ";
+      //System.out.println(inside + " - " + i + " = " + Arrays.toString(array2) + Integer.parseInt(array2[i]));
+        inside.set(Integer.parseInt(array2[i])," ");
+        //System.out.println(inside + " - " + Arrays.toString(array2) + " - " + i);
       }
       test = sc.nextLine();
     }
-   System.out.println(outter); 
+   //System.out.println(outter); 
   }
 }
