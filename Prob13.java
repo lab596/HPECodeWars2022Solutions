2e660a6eb83cef5fa8579216d0ed5045f4454663
lab596 <86579136+lab596@users.noreply.github.com>
2022-03-18 10:54:42 -0700

    Update Prob13.java

--- Prob13.java
+++ Prob13.java
@@ -7,7 +7,12 @@ class Main {
   public static String[] format(ArrayList<String> nums){
     String[] nums2  = new String[nums.size()];
     for(int i=0;i<nums.size();i++){
-      nums2[i] = "#";
+      if(nums.get(i)==" "){
+        nums2[i]=" ";
+      }
+      else{
+        nums2[i] = "#";
+      }
     }
     return nums2;
   }
@@ -25,21 +30,31 @@ class Main {
     }
     String test;
     while(!(test = sc.nextLine()).equals("END")){
-      System.out.println("test: " + test);
+  
+      //System.out.println("test: " + test);
       //String str = sc.nextLine();
       String array1[]= test.split(":");
-      System.out.println(Arrays.toString(array1));
+      //System.out.println(Arrays.toString(array1));
       int ycoord = Integer.parseInt(array1[0].strip());
       String array2[]= array1[1].strip().split(" ");
       //System.out.println(outter.get(ycoord));
+      ArrayList<String> inside = new ArrayList<String>(Arrays.asList(format(outter.get(ycoord))));
       for(int i=0;i<array2.length;i++){
-        ArrayList<String> inside = new ArrayList<String>(Arrays.asList(format(outter.get(ycoord))));
+        //ArrayList<String> inside = new ArrayList<String>(Arrays.asList(format(outter.get(ycoord))));
       //System.out.println(inside + " - " + i + " = " + Arrays.toString(array2) + Integer.parseInt(array2[i]));
         inside.set(Integer.parseInt(array2[i])," ");
         //System.out.println(inside + " - " + Arrays.toString(array2) + " - " + i);
+        //System.out.println(inside);
       }
+      //System.out.println(inside);
+      outter.set(ycoord,inside);
+      System.out.println(outter); 
       test = sc.nextLine();
     }
+    for(int i=0; i< outter.size();i++){
+      System.out.println(outter.get(i));
+    }
+    //System.out.println(outter); 
    //System.out.println(outter); 
   }
 }
