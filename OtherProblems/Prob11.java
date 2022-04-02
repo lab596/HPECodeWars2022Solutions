81c498096e0b2ab8c63dd14b13a375ce9f647f57
lab596 <86579136+lab596@users.noreply.github.com>
2022-04-02 12:23:07 -0700

    Update Prob11.java

--- OtherProblems/Prob11.java
+++ OtherProblems/Prob11.java
@@ -1,4 +1,66 @@
-// What I hav so far:
+/*
+You've heard of Pig Latin, but did you know chipmunks have
+their own version of that? They call it Squeak Latin!
+Input
+You will receive between 3-9 lines of text. Each line will be no
+longer than 200 characters. Input stops when you encounter
+the word END on a line by itself.
+How did you guys?
+We're talking chipmunks, Dave.
+We can get out of a cat carrier.
+Not even hard to do.
+END
+Output
+Change all words beginning with a consonant by moving the
+first letter to the end of the word, then add "-squeak" after that.
+If a word begins with a consonant and is only two letters long, leave it unchanged. Else if a word begins with a
+vowel (aA,eE,iI,oO,uU), simply add "-m" to the word. Do not worry about correcting the CaSe of moved letters
+(we're talking about chipunks here, they aren't picky). Preserve all punctuation (punctuation can include: commas
+(,) exclamation marks (!) periods (.) and question marks (?)).
+owH-squeak idd-squeak ouy-squeak uysg-squeak?
+e'reW-squeak alkingt-squeak hipmunksc-squeak, aveD-squeak.
+We anc-squeak etg-squeak out-m of-m a-m atc-squeak arrierc-squeak.
+otN-squeak even-m ardh-squeak to do.
+Additional Examples
+Input 1 Output 1
+That armors too strong for
+blasters.
+Rogue Group, use your harpoons
+and tow cables.
+Go for the legs.
+It might be our only chance of
+stopping them.
+END
+hatT-squeak armors-m oot-squeak trongs-squeak orf-squeak
+lastersb-squeak.
+ogueR-squeak roupG-squeak, use-m oury-squeak arpoonshsqueak and-m owt-squeak ablesc-squeak.
+Go orf-squeak het-squeak egsl-squeak.
+It-m ightm-squeak be our-m only-m hancec-squeak of-m
+toppings-squeak hemt-squeak.
+Input 2 Output 2
+Before the network, there was
+the fleet.
+Before diplomacy, there were
+soldiers.
+Our influence stopped the
+rachni, but before that, we
+held the line.
+Our influence stopped the
+krogan, but before that, we
+held the line!
+Our influence will stop Saren!
+in the battle today, we will
+hold the line!
+END
+eforeB-squeak het-squeak etworkn-squeak, heret-squeak
+asw-squeak het-squeak leetf-squeak.
+eforeB-squeak iplomacyd-squeak, heret-squeak erew-squeak
+oldierss-squeak.
+Our-m influence-m toppeds-squeak het-squeak achnirsqueak, utb-squeak eforeb-squeak hatt-squeak, we eldhsqueak het-squeak inel-squeak.
+Our-m influence-m toppeds-squeak het-squeak roganksqueak, utb-squeak eforeb-squeak hatt-squeak, we eldhsqueak het-squeak inel-squeak!
+Our-m influence-m illw-squeak tops-squeak arenS-squeak!
+in-m het-squeak attleb-squeak odayt-squeak, we illwsqueak oldh-squeak het-squeak inel-squeak!
+*/
 
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -14,7 +76,6 @@ class Main {
   }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    //ArrayList<Character> vowels = new ArrayList<>(Arrays.asList('a','e','i','o','u'));
     String test;
     ArrayList <ArrayList<String>> wordsp = new ArrayList<ArrayList<String>>();
     while(!(test = sc.nextLine()).equals("END")){
@@ -25,17 +86,14 @@ class Main {
       }
       for(int i=0; i< words.size();i++){
         String a = words.get(i);
-        boolean punctuationb = false;
         boolean punctuationa = false;
-        if(Main.isPunctuation(a.charAt(0))){
-          punctuationb = true;
-        }
         if(Main.isPunctuation(a.charAt(a.length()-1))){
           punctuationa = true;
         }
         if(Main.isVowel(a.charAt(0))){
           if(punctuationa==true){
             a = a.substring(0,a.length()-1) + "-m" + a.substring(a.length()-1);
+            words.set(i,a);
           }
           else{
             a = a +"-m";
@@ -43,14 +101,9 @@ class Main {
           }
         }
         else{
-          if(punctuationa==true && punctuationb==true){
-            a = a.substring(0,1) + a.substring(1,a.length()-1) + a.charAt(1) + "-squeak" + a.substring(a.length()-1);
-          }
-          else if(punctuationa==true){
-            a = a.substring(0,1) + a.substring(1,a.length()) + a.charAt(1) + "-squeak";
-          }
-          else if(punctuationb==true){
+          if(punctuationa==true){
             a = a.substring(1,a.length()-1) + a.charAt(0) + "-squeak" + a.substring(a.length()-1);
+            words.set(i,a);
           }
           else{
             a = a.substring(1,a.length()) + a.charAt(0) + "-squeak";
@@ -59,9 +112,13 @@ class Main {
         }
       }
       wordsp.add(words);
-      //System.out.println(words);
+
     }
-    System.out.println(wordsp);
+    for(ArrayList<String> a : wordsp){
+      for(int i=0;i<a.size()-1;i++){
+        System.out.print(a.get(i) + " ");
+      }
+      System.out.println(a.get(a.size()-1));
+    }
   }
-  
 }
